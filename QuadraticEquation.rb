8c013acb3a2ac21aac2ca367739e626bfce8151a
H0QW010XZ <CloserCloseCloser@gmail.com>
2018-11-07 09:49:45 -0500

    fix

--- QuadraticEquation.rb
+++ QuadraticEquation.rb
@@ -11,10 +11,10 @@ d = b**2 - 4 * a * c
 
 if d > 0 
   sqrt_d = Math.sqrt(d)
-  x1 = (-b + sqrt_d)/ (2 * a)
-  x2 = (-b - sqrt_d)/ (2 * a)
+  x1 = (-b + sqrt_d) / (2 * a)
+  x2 = (-b - sqrt_d) / (2 * a)
   puts "x1 = #{x1}, x2 = #{x2}"
-elsif d == 0
+elsif d.zero?
   x = -b / (2.0 * a)
   puts "x1 = x2 = #{x}" 
 else 
