3d2abbccc1d601d4be015ff3cc5f7178cacd96de
H0QW010XZ <CloserCloseCloser@gmail.com>
2018-11-07 09:13:31 -0500

    fix

--- QuadraticEquation.rb
+++ QuadraticEquation.rb
@@ -7,11 +7,12 @@ b = gets.chomp.to_f
 puts 'Введите c: '
 c = gets.chomp.to_f
 
-d = Math.sqrt(b**2 - 4 * a * c)
+d = b**2 - 4 * a * c
 
 if d > 0 
-    x1 = (-b + d)/ (2 * a)
-    x2 = (-b - d)/ (2 * a)
+    sqrt_d = Math.sqrt(d)
+    x1 = (-b + sqrt_d)/ (2 * a)
+    x2 = (-b - sqrt_d)/ (2 * a)
     puts "x1 = #{x1}, x2 = #{x2}"
 elsif d == 0
     x = -b / (2.0 * a)
