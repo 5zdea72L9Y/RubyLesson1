353a4652b40a8c5506032003df3b394aeafcfee8
H0QW010XZ <CloserCloseCloser@gmail.com>
2018-11-07 09:32:52 -0500

    fix

--- QuadraticEquation.rb
+++ QuadraticEquation.rb
@@ -10,13 +10,13 @@ c = gets.chomp.to_f
 d = b**2 - 4 * a * c
 
 if d > 0 
-    sqrt_d = Math.sqrt(d)
-    x1 = (-b + sqrt_d)/ (2 * a)
-    x2 = (-b - sqrt_d)/ (2 * a)
-    puts "x1 = #{x1}, x2 = #{x2}"
+  sqrt_d = Math.sqrt(d)
+  x1 = (-b + sqrt_d)/ (2 * a)
+  x2 = (-b - sqrt_d)/ (2 * a)
+  puts "x1 = #{x1}, x2 = #{x2}"
 elsif d == 0
-    x = -b / (2.0 * a)
-    puts "x1 = x2 = #{x}" 
+  x = -b / (2.0 * a)
+  puts "x1 = x2 = #{x}" 
 else 
-    puts 'Корней нет'
+  puts 'Корней нет'
 end 
--- Weight.rb
+++ Weight.rb
@@ -7,7 +7,7 @@ weight = gets.chomp.to_f
 ideal_weight = weight - 110
 
 if ideal_weight > 0
-    puts "Ваше имя #{name}, ваш идеальный вес #{ideal_weight} кг"
+  puts "Ваше имя #{name}, ваш идеальный вес #{ideal_weight} кг"
 else 
-    puts 'Ваше имя #{name}, ваш вес уже оптимальный'
+  puts 'Ваше имя #{name}, ваш вес уже оптимальный'
 end 
